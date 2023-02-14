be44c66b7fa99f3c82609d37ea7f7886479c31d8
yichang <740123730@qq.com>
2023-02-14 11:05:32 +0800

    1

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('D:\\wiki\\wiki\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('D:\\wiki\\wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('D:\\wiki\\wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('D:\\wiki\\wiki\\src\\css\\custom.css'),
+];
